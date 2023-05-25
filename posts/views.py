eb89041ff3f06967c4c9a488c896614082c24d95
Van-Excel <vanexcel.investments@gmail.com>
2023-05-25 08:31:29 +0000

    added project level and custom permissions

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,20 @@
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
+
 
 # Create your views here.
 
 
 class PostsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostsListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
